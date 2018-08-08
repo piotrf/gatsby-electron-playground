ec40e48d6368883311886f6c14a93b7e417bbd39
Piotr F <piotr@piotrf.pl>
2018-08-08 18:11:31 +0200

    Set up mac release script and a first dist.

---
+++ dist/electron-gatsby-playground-darwin-x64/electron-gatsby-playground.app/Contents/Resources/app/gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: `https://moglistudio.com`,
+    title: `moglistudio.com-v1 (2018)`
+  },
+  plugins: [
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-next`,
+    `gatsby-plugin-styled-components`
+  ]
+};
